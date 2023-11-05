9b85c67a13da09958ad52a8e13c50c677669bba7
Denis Rocha <dns_rocha@hotmail.com>
2023-11-05 18:47:12 -0300

    include-screen-gameOver

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -4,7 +4,7 @@ let numMax = 100;
 const elementoMenor = document.getElementById('menor-valor').innerHTML = numMin;
 const elementoMaior = document.getElementById('maior-valor').innerHTML = numMax;
 
-const numeroSecreto = numeroAleatorio();
+const numeroSecreto = 12;
 
 function numeroAleatorio(){
     return parseInt(Math.random() * numMax + 1);
--- app/validacoes.js
+++ app/validacoes.js
@@ -2,10 +2,18 @@ function verificandoSeOChutePossuiUmValorValido(chute){
     let numero = +chute
     
     if(chuteForInvalido(numero)){
-        elementoChute.innerHTML += '<div><i class="fa-solid fa-face-sad-cry"></i> Valor inválido</div>';
-        return 
+        if(chute.toUppercase() === "GAME OVER"){
+
+            document.body.innerHTML = `<h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>`
+            document.body.style.backgroundColor = "black";
+        }else{
+            elementoChute.innerHTML += '<div><i class="fa-solid fa-face-sad-cry"></i> Valor inválido</div>';
+         }
+  
     }
-   if(chute < numMin || chute > numMax){
+   if(numeroMaiorOuMenorQueOValorPermitido(chute)){
         elementoChute.innerHTML += '<div><i class="fa-solid fa-face-sad-cry"></i> Valor inválido</div>';
         return 
    }
@@ -14,12 +22,17 @@ function verificandoSeOChutePossuiUmValorValido(chute){
     document.body.innerHTML = `<h2><i class="fa-solid fa-face-smile-wink"></i> Você Acertou!</h2>
     <h3>O número secreto era ${numeroSecreto}</h3>
     <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>`;
+
    } else if (numero > numeroSecreto){
         elementoChute.innerHTML += `
         <div>o numero secreto é maior <i class="fa-solid fa-angles-up"></i></div>`;
    } else {
     elementoChute.innerHTML += `<div>o numero secreto é menor <i class="fa-solid fa-angles-down"></i></div>`
    }
+}
+
+function numeroMaiorOuMenorQueOValorPermitido(chute) {
+    return chute < numMin || chute > numMax;
 }
 
 function chuteForInvalido(numero) {
